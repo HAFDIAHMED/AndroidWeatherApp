88e3b133df34b79868c6a8e8ad013067892ce039
HAFDIAHMED <ahafdi62@gmail.com>
2021-12-01 18:15:25 +0100

    android weather

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -5,14 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn_getId,btn_getName,get_Weather;
+    Button btn_getId,btn_getName,btn_getWeather;
     EditText cityName_textInput;
+    ListView list_view_weather;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        btn_getId=findViewById(R.id.btn_cityId);
+        btn_getName=findViewById(R.id.btn_cityName);
+        btn_getWeather=findViewById(R.id.btn_cityWeather);
     }
 }
