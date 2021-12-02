a9feea2d7f2bcdbfc0b5c676cc647cb86257eeb5
HAFDIAHMED <ahafdi62@gmail.com>
2021-12-02 11:52:12 +0100

    android weather

--- app/src/test/java/com/example/weatherapp/WeatherReportModel.java
+++ app/src/test/java/com/example/weatherapp/WeatherReportModel.java
@@ -2,6 +2,7 @@ package com.example.weatherapp;
 
 public class WeatherReportModel {
 
+
     private int id ;
     private String weather_state_name;
     private String weather_state_abbr;
@@ -14,8 +15,25 @@ public class WeatherReportModel {
     private float wind_speed;
     private float wind_direction;
     private float air_pressure;
-    private float humidity ;
+    private int humidity ;
     private float visibility;
-    private float predictability;
+    private int predictability;
 
+    public WeatherReportModel(int id, String weather_state_name, String weather_state_abbr, String wind_direction_compass, String created, String applicable_date, float min_temp, float max_temp, float the_temp, float wind_speed, float wind_direction, float air_pressure, int humidity, float visibility, int predictability) {
+        this.id = id;
+        this.weather_state_name = weather_state_name;
+        this.weather_state_abbr = weather_state_abbr;
+        this.wind_direction_compass = wind_direction_compass;
+        this.created = created;
+        this.applicable_date = applicable_date;
+        this.min_temp = min_temp;
+        this.max_temp = max_temp;
+        this.the_temp = the_temp;
+        this.wind_speed = wind_speed;
+        this.wind_direction = wind_direction;
+        this.air_pressure = air_pressure;
+        this.humidity = humidity;
+        this.visibility = visibility;
+        this.predictability = predictability;
+    }
 }
