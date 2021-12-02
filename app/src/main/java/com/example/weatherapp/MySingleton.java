d34db5401c10e6c962b16e38239da6e9ca28263d
HAFDIAHMED <ahafdi62@gmail.com>
2021-12-02 10:16:45 +0100

    android weather

--- app/src/main/java/com/example/weatherapp/MySingleton.java
+++ app/src/main/java/com/example/weatherapp/MySingleton.java
@@ -15,21 +15,7 @@ public class MySingleton {
         ctx = context;
         requestQueue = getRequestQueue();
 
-        imageLoader = new ImageLoader(requestQueue,
-                new ImageLoader.ImageCache() {
-                    private final LruCache<String, Bitmap>
-                            cache = new LruCache<String, Bitmap>(20);
-
-                    @Override
-                    public Bitmap getBitmap(String url) {
-                        return cache.get(url);
-                    }
-
-                    @Override
-                    public void putBitmap(String url, Bitmap bitmap) {
-                        cache.put(url, bitmap);
-                    }
-                });
+        
     }
 
     public static synchronized MySingleton getInstance(Context context) {
