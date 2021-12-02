e89144c8a76c325173ad7c02a49ab64c4fa5e2e0
HAFDIAHMED <ahafdi62@gmail.com>
2021-12-02 11:45:12 +0100

    android weather

--- app/src/test/java/com/example/weatherapp/WeatherReportModel.java
+++ app/src/test/java/com/example/weatherapp/WeatherReportModel.java
@@ -1,5 +1,21 @@
 package com.example.weatherapp;
 
 public class WeatherReportModel {
+    "id": 4753582973779968,
+            "weather_state_name": "Showers",
+            "weather_state_abbr": "s",
+            "wind_direction_compass": "NNW",
+            "created": "2021-12-02T09:59:02.959789Z",
+            "applicable_date": "2021-12-02",
+            "min_temp": 0.87,
+            "max_temp": 4.53,
+            "the_temp": 3.7199999999999998,
+            "wind_speed": 9.007910778066757,
+            "wind_direction": 330.1887497987284,
+            "air_pressure": 1011.0,
+            "humidity": 71,
+            "visibility": 10.736051459476656,
+            "predictability": 73
+    
 
 }
