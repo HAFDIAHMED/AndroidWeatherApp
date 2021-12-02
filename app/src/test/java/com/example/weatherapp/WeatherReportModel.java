9692ade2b1e5a7a39cd1342f18a3d8755355d9d9
HAFDIAHMED <ahafdi62@gmail.com>
2021-12-02 11:53:57 +0100

    android weather

--- app/src/test/java/com/example/weatherapp/WeatherReportModel.java
+++ app/src/test/java/com/example/weatherapp/WeatherReportModel.java
@@ -36,4 +36,145 @@ public class WeatherReportModel {
         this.visibility = visibility;
         this.predictability = predictability;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getWeather_state_name() {
+        return weather_state_name;
+    }
+
+    public String getWeather_state_abbr() {
+        return weather_state_abbr;
+    }
+
+    public String getWind_direction_compass() {
+        return wind_direction_compass;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public String getApplicable_date() {
+        return applicable_date;
+    }
+
+    public float getMin_temp() {
+        return min_temp;
+    }
+
+    public float getMax_temp() {
+        return max_temp;
+    }
+
+    public float getThe_temp() {
+        return the_temp;
+    }
+
+    public float getWind_speed() {
+        return wind_speed;
+    }
+
+    public float getWind_direction() {
+        return wind_direction;
+    }
+
+    public float getAir_pressure() {
+        return air_pressure;
+    }
+
+    public int getHumidity() {
+        return humidity;
+    }
+
+    public float getVisibility() {
+        return visibility;
+    }
+
+    public int getPredictability() {
+        return predictability;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setWeather_state_name(String weather_state_name) {
+        this.weather_state_name = weather_state_name;
+    }
+
+    public void setWeather_state_abbr(String weather_state_abbr) {
+        this.weather_state_abbr = weather_state_abbr;
+    }
+
+    public void setWind_direction_compass(String wind_direction_compass) {
+        this.wind_direction_compass = wind_direction_compass;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+
+    public void setApplicable_date(String applicable_date) {
+        this.applicable_date = applicable_date;
+    }
+
+    public void setMin_temp(float min_temp) {
+        this.min_temp = min_temp;
+    }
+
+    public void setMax_temp(float max_temp) {
+        this.max_temp = max_temp;
+    }
+
+    public void setThe_temp(float the_temp) {
+        this.the_temp = the_temp;
+    }
+
+    public void setWind_speed(float wind_speed) {
+        this.wind_speed = wind_speed;
+    }
+
+    public void setWind_direction(float wind_direction) {
+        this.wind_direction = wind_direction;
+    }
+
+    public void setAir_pressure(float air_pressure) {
+        this.air_pressure = air_pressure;
+    }
+
+    public void setHumidity(int humidity) {
+        this.humidity = humidity;
+    }
+
+    public void setVisibility(float visibility) {
+        this.visibility = visibility;
+    }
+
+    public void setPredictability(int predictability) {
+        this.predictability = predictability;
+    }
+
+    @Override
+    public String toString() {
+        return "WeatherReportModel{" +
+                "id=" + id +
+                ", weather_state_name='" + weather_state_name + '\'' +
+                ", weather_state_abbr='" + weather_state_abbr + '\'' +
+                ", wind_direction_compass='" + wind_direction_compass + '\'' +
+                ", created='" + created + '\'' +
+                ", applicable_date='" + applicable_date + '\'' +
+                ", min_temp=" + min_temp +
+                ", max_temp=" + max_temp +
+                ", the_temp=" + the_temp +
+                ", wind_speed=" + wind_speed +
+                ", wind_direction=" + wind_direction +
+                ", air_pressure=" + air_pressure +
+                ", humidity=" + humidity +
+                ", visibility=" + visibility +
+                ", predictability=" + predictability +
+                '}';
+    }
 }
